a269043b08808baf05855545809d32d106813b55
Rishab Mandal <102305005+RishabMandal@users.noreply.github.com>
2025-01-08 11:42:58 +0530

    Added alert in Navbar

--- app/components/Home/page.jsx
+++ app/components/Home/page.jsx
@@ -62,7 +62,7 @@ const page = () => {
 
   return (
     <div className="bg-[#1d2440] min-h-screen h-full text-white">
-      <Modal
+      {/* <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
@@ -105,7 +105,7 @@ const page = () => {
             </button>
           </div>
         </div>
-      </Modal>
+      </Modal> */}
       <div className="flex h-full">
         <div
           ref={leftDivRef}
@@ -216,6 +216,53 @@ const page = () => {
           ref={rightDivRef}
           className={`flex-1 flex flex-wrap justify-start items-start p-5 gap-5 h-fit`}
         >
+          {alertCameraData && (
+            <div className="relative min-w-[20vw] flex-1 h-fit">
+              {alertCameraData.video === false ? (
+                <Image
+                  src={
+                    CameraView === "Thermal"
+                      ? alertCameraData.thermalImage
+                      : CameraView === "Normal"
+                      ? alertCameraData.normalImage
+                      : CameraView === "Night"
+                      ? alertCameraData.nightImage
+                      : null
+                  }
+                  alt="Camera Img"
+                  onClick={() => {
+                    setCurrentCameraData(alertCameraData);
+                    router.push("/components/DetailedVideoSection");
+                  }}
+                  className="relative w-full h-auto border-2 border-red-600 bg-black duration-200 rounded-lg object-contain cursor-pointer hover:scale-105"
+                />
+              ) : (
+                <video
+                  // controls
+                  loop
+                  autoPlay
+                  src={
+                    CameraView === "Thermal"
+                      ? alertCameraData.thermalImageAnnotated
+                      : CameraView === "Normal"
+                      ? alertCameraData.normalImage
+                      : CameraView === "Night"
+                      ? alertCameraData.nightImageAnnotated
+                      : null
+                  }
+                  alt="Camera Img"
+                  onClick={() => {
+                    setCurrentCameraData(alertCameraData);
+                    router.push("/components/DetailedVideoSection");
+                  }}
+                  className="relative w-full h-auto border-2 border-red-600 bg-black duration-200 rounded-lg object-contain cursor-pointer hover:scale-105"
+                />
+              )}
+              <div className="absolute z-30 font-semibold top-2 left-2 bg-[#2b4075] text-white text-sm p-1 rounded">
+                Zone {alertCameraData.id}
+              </div>
+            </div>
+          )}
           {data.slice(5, 9).map((item, index) => (
             <div key={index} className="relative min-w-[20vw] flex-1 h-fit">
               {item.video === false ? (
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,52 +6,72 @@ import Image from "next/image";
 import flag from "../assets/maps/flag.jpg";
 
 const Navbar = () => {
-  const { isAuthUser, currentCameraData, data, setAlertCameraData } =
-    useContext(GlobalContext);
+  const {
+    isAuthUser,
+    currentCameraData,
+    data,
+    alertCameraData,
+    setAlertCameraData,
+  } = useContext(GlobalContext);
   const router = useRouter();
-  return isAuthUser === true ? (
-    <div className="bg-[#2f3b61] p-1">
+  return isAuthUser === true || isAuthUser === false ? (
+    <div className={`${alertCameraData ? "bg-[#E83215]" : "bg-[#2f3b61]"} p-1`}>
       <div className="flex flex-row justify-between">
         <div className="font-bold pl-[0.35rem] text-xl flex flex-row w-fit items-center justify-start">
-          <Image src={flag} alt="flag" className="h-[1rem] object-contain w-fit pr-2" />
+          <Image
+            src={flag}
+            alt="flag"
+            className="h-[1rem] object-contain w-fit pr-2"
+          />
           {/* {currentCameraData && currentCameraData.id != undefined
             ? `Camera ${currentCameraData.id}`
             : "Border Surveillance System"} */}
           <div>Border Surveillance System</div>
         </div>
-        <div>
-          <div className="flex flex-row">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-7 cursor-pointer mx-1 stroke-[#2ea1f1] py-1 px-1"
-              onClick={() => router.push("/components/Home")}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-7 cursor-pointer mx-1 stroke-[#2ea1f1] py-1 px-1"
-              onClick={() => router.push("/components/TvView")}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z"
-              />
-            </svg>
-            {/* <svg
+        {alertCameraData && (
+          <div className="font-bold pl-[0.35rem] text-xl flex flex-row gap-1 w-fit items-center justify-start">
+            <div>Alert!!!</div>
+            <div>Threat found at Zone {alertCameraData?.id}</div>
+          </div>
+        )}
+        {isAuthUser && (
+          <div>
+            <div className="flex flex-row">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className={`size-7 cursor-pointer mx-1 ${
+                  !alertCameraData && "stroke-[#2ea1f1]"
+                } py-1 px-1`}
+                onClick={() => router.push("/components/Home")}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className={`size-7 cursor-pointer mx-1 ${
+                  !alertCameraData && "stroke-[#2ea1f1]"
+                } py-1 px-1`}
+                onClick={() => router.push("/components/TvView")}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z"
+                />
+              </svg>
+              {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -65,37 +85,46 @@ const Navbar = () => {
                 d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
               />
             </svg> */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-7 cursor-pointer mx-1 stroke-[#2ea1f1] py-1 px-1"
-              onClick={() => setAlertCameraData(data[0])}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-7 cursor-pointer mx-1 stroke-[#2ea1f1] py-1 px-1"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-              />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className={`size-7 cursor-pointer mx-1 ${
+                  !alertCameraData && "stroke-[#2ea1f1]"
+                } py-1 px-1`}
+                onClick={() =>
+                  alertCameraData
+                    ? setAlertCameraData()
+                    : setAlertCameraData(data[7])
+                }
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className={`size-7 cursor-pointer mx-1 ${
+                  !alertCameraData && "stroke-[#2ea1f1]"
+                } py-1 px-1`}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                />
+              </svg>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   ) : (
