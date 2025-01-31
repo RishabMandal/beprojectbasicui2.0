68aced9a62c897b0519f7d95665e32733b381579
Rishab Mandal <102305005+RishabMandal@users.noreply.github.com>
2025-01-31 20:31:21 +0530

    Added select box

--- app/components/ChatBot/page.jsx
+++ app/components/ChatBot/page.jsx
@@ -91,9 +91,90 @@ const page = () => {
   };
   const [rating, setRating] = useState(0);
 
+  const [popoverVisible, setPopoverVisible] = useState(false);
+  const [modelSelectOption, setModelSelectOption] = useState("SeekDeep v1");
+  const togglePopover = () => {
+    setPopoverVisible((prev) => !prev);
+  };
+
   return (
     <div className="min-h-screen p-5 bg-[#25314f]">
-      {/* <div>Answer:</div> */}
+      {/* Select Box with Popover */}
+      <div className="relative sticky top-0 bg-[#25314f]">
+        <button
+          className="py-2 px-4 text-white flex flex-row gap-3"
+          onClick={togglePopover}
+        >
+          {modelSelectOption}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </button>
+
+        {/* Popover */}
+        {popoverVisible && (
+          <div className="absolute bg-[#25314f] border rounded-xl shadow-md mt-2 w-56 z-10 p-3">
+            <ul>
+              <li
+                onClick={() => setModelSelectOption("SeekDeep v1")}
+                className="py-2 rounded-xl px-4 flex flex-row justify-between gap-3 items-center hover:bg-blue-600 cursor-pointer"
+              >
+                <p>SeekDeep v1</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6 w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+                  />
+                </svg>
+              </li>
+              <li
+                onClick={() => setModelSelectOption("SeekDeep Pro")}
+                className="py-2 rounded-xl px-4 flex flex-row justify-between gap-3 items-center hover:bg-blue-600 cursor-pointer"
+              >
+                <p>SeekDeep Pro</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6 w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z"
+                  />
+                </svg>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
       <div className="flex flex-row gap-5">
         {/* <div className="border flex-1 min-h-[70vh]">
           answ
